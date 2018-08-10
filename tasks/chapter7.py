ba282b7ae44f3f7530f6fe5301d70839b26f77ea
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-08-10 20:26:02 +0300

    Task 178b is completed

--- tasks/chapter7.py
+++ tasks/chapter7.py
@@ -1,1 +1,13 @@
 ﻿
+def task_178b(numbers):
+    """
+    Accepts list of natural numbers
+    Returns amount of numbers that are divisible by 3 and not divisible by 5.
+    :param nums: [1, 52, 12, 25, 5, 2, 3]
+    :return: 2
+    """
+    quantity = 0
+    for num in numbers:
+        if num % 3 == 0 and num % 5 != 0:
+            quantity += 1
+    return quantity
