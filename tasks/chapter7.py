f1a1cd7dd6602f6dd2e54cf0e582dfe89280c1d5
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-08-10 20:46:30 +0300

    Task #226 is completed

--- tasks/chapter7.py
+++ tasks/chapter7.py
@@ -11,3 +11,16 @@ def task_178b(numbers):
         if num % 3 == 0 and num % 5 != 0:
             quantity += 1
     return quantity
+
+
+def task_226(n, m):
+    """
+    Accepts two natural numbers
+    Returns list of numbers that are divisible by both n and m and are less or equal m*n
+    :param n: 12
+    :param m: 3
+    :return: [36, 24, 12]
+    """
+    bigger = max(n, m)
+    smaller = min(n, m)
+    return [num for num in range(m*n, bigger-1, -bigger) if num % smaller == 0]
