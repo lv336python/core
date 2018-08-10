7eb728e52e5fb9ea0732e7f8a082bbbdda68758d
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-08-10 20:12:13 +0300

    Deleted casts to integer because ints are expected as input

--- tasks/chapter4.py
+++ tasks/chapter4.py
@@ -3,9 +3,9 @@
 def task_87(n, m):
     if n < 0:
         return None
-    elif m <= 0:
+    elif m < 1:
         return None
     elif m > len(str(n)):
         return None
-    return sum([int(i) for i in str(int(n))[-m::]])
+    return sum([int(i) for i in str(n)[-m::]])
 
