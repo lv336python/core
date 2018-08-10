ebff5871018f76f15312637b3ad0211f71ca16db
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-08-10 20:06:18 +0300

    Task 87 is completed

--- tasks/chapter4.py
+++ tasks/chapter4.py
@@ -1,1 +1,18 @@
 ﻿
+
+def task_87(n, m):
+    if n < 0:
+        return None
+    elif m <= 0:
+        return None
+    elif m > len(str(n)):
+        return None
+    return sum([int(i) for i in str(int(n))[-m::]])
+
+
+if __name__ == '__main__':
+    print(task_87(32636, 3))
+    print(task_87(32636, 5))
+    print(task_87(32636, 0))
+    print(task_87(32636, 85))
+
