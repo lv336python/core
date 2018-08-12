0486b10394b604294de91c65e622d98fb633b73d
srhstas <sergii.stasiuk@gmail.com>
2018-08-12 13:26:41 +0300

    added missing chapter files and complete tasks for chapters 4 and 7

--- tasks/chapter4.py
+++ tasks/chapter4.py
@@ -1,1 +1,9 @@
-﻿
+def  task_88_c(num):
+	'''
+	Function swap last and first digit of given number.
+	Input have to be an integer.
+	Example: 3*****5 >> 5*****3
+	'''
+	iter_num = list(str(num)) #making number iterable
+	iter_num[0], iter_num[-1] = iter_num[-1], iter_num[0] #swapping digits
+	return int(''.join(iter_num)) #output type is the same as input
--- tasks/chapter7.py
+++ tasks/chapter7.py
@@ -1,1 +1,15 @@
-﻿
+﻿def task_243_b(num):
+	'''
+	Function finds all pairs of x, y for which x^2 + y^2 = num
+	and x>=y.
+	Return will be a list of tuples with pair x, y
+	'''
+	from math import sqrt 
+	max_n = int(sqrt(num)) # will limit range and reduce check time
+	pairs = []
+	for x in range(1,max_n):
+		for y in range(1,max_n):
+			if x**2 + y**2 == num and x>=y: 
+				pair = (x,y)
+				pairs.append(pair)
+	return pairs
